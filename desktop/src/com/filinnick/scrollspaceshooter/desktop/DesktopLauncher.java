6183be16e77790e7cf55fa6676940fe0a4a4202f
INV000077WIN\User <developer19@vr-arsoft.com>
2021-11-10 10:55:20 +0400

    Development started.

---
+++ desktop/src/com/filinnick/scrollspaceshooter/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.filinnick.scrollspaceshooter.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.filinnick.scrollspaceshooter.ScrollSpaceShooterGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ScrollSpaceShooterGame(), config);
+	}
+}
